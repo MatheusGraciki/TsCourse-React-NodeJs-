741db6ff13a4e96ba6de594b41576e99c092a5aa
Matheus Graciki <matheusgraciki7@gmail.com>
2023-10-08 20:08:18 -0300

    Heranças, explicação da diferença entre classe normal e abstrata, a do exemplo é uma abstrata

--- app.ts
+++ app.ts
@@ -1,3 +1,18 @@
+
+/*      
+
+    Classe Normal:
+Uma classe normal pode ser instanciada para criar objetos.
+Ela pode conter métodos e propriedades concretos (ou seja, métodos com implementações definidas).
+Pode ser usada diretamente para criar instâncias e herdar de outras classes.   
+
+    Classe Abstrata:
+Uma classe abstrata não pode ser instanciada diretamente. Ela serve como um modelo para outras classes, ou seja, é projetada para ser herdada por outras classes.
+Pode conter métodos abstratos (métodos sem implementações) que devem ser implementados pelas classes que herdam dela.
+Pode conter métodos concretos que são herdados pelas subclasses, mas também podem ser sobrescritos.
+Geralmente usada quando você deseja definir um conjunto de métodos e propriedades comuns para várias classes relacionadas.
+*/
+
 abstract class Account{
     name: string;
     accountNumber: number;
@@ -27,13 +42,26 @@ class PeopleAccount  extends Account{
     doc_id: number;
 
     constructor(doc_id: number, name: string, accountNumber: number) {
+        // o super construtor é responsavel por inicializar a classe que estamos extendendo, no caso a Account 
         super(name, accountNumber);
         this.doc_id = doc_id;
+    }
+}
+
+class CompanyAccount extends Account{
+    constructor(name: string, accountNumber: number) {
+        super(name, accountNumber);
+    }
+
+    getLoan = () => {
+        console.log("voce pegou um emprestimo")
     }
 }
 
 const peopleAccount = new PeopleAccount(1, 'Juan', 2)
 console.log(peopleAccount)
 
+const companyAccount: CompanyAccount = new CompanyAccount('Juan', 1)
+companyAccount.getLoan
 
 
